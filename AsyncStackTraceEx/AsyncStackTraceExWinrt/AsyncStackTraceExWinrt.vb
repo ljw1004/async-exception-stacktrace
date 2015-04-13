f58bf45d37caf75db0134343c7810f302cc06a13
lwischik <lu@wischik.com>
2015-04-13 08:11:28 -0700

    Added extension methods on IAsyncAction/IAsyncOpertion types, in a separate WinRT-specific PCL, and updated the NuGet to deploy this specific PCL into appropriate projects.

---
+++ AsyncStackTraceEx/AsyncStackTraceExWinrt/AsyncStackTraceExWinrt.vb
@@ -0,0 +1,25 @@
+﻿Namespace Global
+    Public Module AsyncStackTraceWinrtExtensions
+
+        <Extension>
+        Public Function Log(Of T)(task As IAsyncOperation(Of T),
+                             Optional label As String = Nothing,
+                             Optional arg As Object = Nothing,
+                             <CallerMemberName> Optional member As String = "",
+                             <CallerLineNumber> Optional line As Integer = 0,
+                             <CallerFilePath> Optional path As String = "") As System.Threading.Tasks.Task(Of T)
+            Return AsyncStackTraceExtensions.Log(task.AsTask(), label, arg, member, line, path)
+        End Function
+
+        <Extension>
+        Public Function Log(task As IAsyncAction,
+                             Optional label As String = Nothing,
+                             Optional arg As Object = Nothing,
+                             <CallerMemberName> Optional member As String = "",
+                             <CallerLineNumber> Optional line As Integer = 0,
+                             <CallerFilePath> Optional path As String = "") As System.Threading.Tasks.Task
+            Return AsyncStackTraceExtensions.Log(task.AsTask(), label, arg, member, line, path)
+        End Function
+
+    End Module
+End Namespace
--- AsyncStackTraceEx/AsyncStackTraceExWinrt/My Project/AssemblyInfo.vb
+++ AsyncStackTraceEx/AsyncStackTraceExWinrt/My Project/AssemblyInfo.vb
@@ -1,6 +1,6 @@
 ﻿Imports System
-Imports System.Resources
 Imports System.Reflection
+Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
@@ -8,13 +8,12 @@ Imports System.Reflection
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("AsyncStackTraceEx")> 
+<Assembly: AssemblyTitle("AsyncStackTraceExWinrt")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("AsyncStackTraceEx")>
-<Assembly: AssemblyCopyright("Copyright © Lucian Wischik 2014")>
+<Assembly: AssemblyProduct("AsyncStackTraceExWinrt")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
 <Assembly: AssemblyTrademark("")> 
-<Assembly: NeutralResourcesLanguage("en")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -29,3 +28,4 @@ Imports System.Reflection
 
 <Assembly: AssemblyVersion("1.0.0.2")> 
 <Assembly: AssemblyFileVersion("1.0.0.2")> 
+<Assembly: ComVisible(false)>
